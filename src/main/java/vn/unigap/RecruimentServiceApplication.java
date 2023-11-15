a50c1f2e4248189f80e10831a3420e1e07f8bb08
Tien Huynh <86528079+TienHuynh25@users.noreply.github.com>
2023-11-15 15:06:10 -0600

    first commit

---
+++ src/main/java/vn/unigap/RecruimentServiceApplication.java
@@ -0,0 +1,13 @@
+package vn.unigap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruimentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecruimentServiceApplication.class, args);
+	}
+
+}
